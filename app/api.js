49e5470cad8ba9e86bd426f5a5c792c505d9ce89
AHalic <sophie.gama@aluno.ufes.br>
2022-12-06 19:53:06 -0300

    :recycle: changes api routes

--- app/api.js
+++ app/api.js
@@ -7,3 +7,11 @@ app.listen(8000, () => {
     console.log('\nServidor rodando na porta 8000')
     console.log('\x1b[36m%s\x1b[0m', 'http://localhost:8000');
 })
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.get('/admin', (req, res) => {
+    res.sendFile(__dirname + '/admin.html')
+})
